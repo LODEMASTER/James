f185ff754c51144ce25b8863a270d4c67b428bdf
unknown <lodemaster>
2022-12-26 11:17:14 +0900

    Main.java

--- src/Test/github.java
+++ src/Test/github.java
@@ -5,6 +5,7 @@ public class github {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("fuck king USA");
+		System.out.println("fuck king korea");
 
 	}
 
